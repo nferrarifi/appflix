45aaa6eeb533f72f3e6d0be7dedbaef5dfec305e
nferrarifi <nahuel.ferrarifisicaro@gmail.com>
2022-09-20 17:09:55 -0300

    Preparation of Auth hook for login and signup feature

---
+++ hooks/useAuth.tsx
@@ -0,0 +1,91 @@
+import { useState, useEffect, useContext, createContext, useMemo } from "react"
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, User } from "firebase/auth"
+import { useRouter } from "next/router"
+import { auth } from "../firebase"
+
+interface AuthProviderProps {
+    children: React.ReactNode
+}
+
+interface IAuth {
+    user: User | null
+    signUp: (email:string, password:string) => Promise<void>
+    signIn: (email:string, password:string) => Promise<void>
+    logout: () => Promise<void>
+    error: string | null
+    loading:boolean
+}
+
+const AuthContext = createContext<IAuth> ({
+    user: null,
+    signUp: async () => {},
+    signIn: async () => {},
+    logout: async () => {},
+    error: null,
+    loading: false
+})
+
+export const AuthProvider = ({children}: AuthProviderProps) => {
+    const [loading, setLoading] = useState(false)
+    const [user, setUser] = useState<User | null> (null)
+    const [error, setError] = useState (null)
+    const [initialLoading, setInitialLoading] = useState(true)
+    const router = useRouter()
+
+    //Persisting the user 
+    useEffect (() => {
+        onAuthStateChanged (auth, (user) => {
+            if (user){
+                //User is logged in
+                setUser (user)
+                setLoading (false)
+            }
+            else {
+                //User is not logged in
+                setUser (null)
+                setLoading (true)
+                router.push ("/login")
+            }
+            setInitialLoading (false)
+        }),
+        [auth]
+    })
+    
+    const signUp = async (email:string, password:string) => {
+        setLoading (true)
+        await createUserWithEmailAndPassword (auth, email, password).then ((userCredential) => {
+            setUser (userCredential.user)
+            router.push("/")
+            setLoading(false)
+        }).catch ((error) => alert (error.message))
+          .finally (() => setLoading(false))
+    }
+
+    const signIn = async (email:string, password:string) => {
+        setLoading (true)
+        await signInWithEmailAndPassword (auth, email, password).then ((userCredential) => {
+            setUser (userCredential.user)
+            router.push("/")
+            setLoading(false)
+        }).catch ((error) => alert (error.message))
+          .finally (() => setLoading(false))
+    }
+
+    const logout = async () => {
+        setLoading (true)
+        signOut(auth).then(() => {
+            setUser(null)
+        }).catch ((error) => alert (error.message))
+          .finally (() => setLoading (false))
+    }
+
+  const memoedValue = useMemo(() => ({
+    user, signIn, signUp, loading, logout, error
+  }), [user, loading])
+
+  return <AuthContext.Provider value={memoedValue}>{!initialLoading && children}</AuthContext.Provider>
+}
+
+export default function useAuth() {
+    return useContext (AuthContext)
+}
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import {useForm} from 'react-hook-form'
+
+interface Inputs {
+  email: string
+  password: string
+}
+
 const login = () => {
+    const [Login, setLogin] = useState(Boolean)
+    const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+    const onSubmit = async ({email, password}) => {
+      if (Login) {
+        //await signIn (email,password)
+      }
+      else {
+        //await  signUp (email,password)
+      }
+    }
+
   return (
     <div className="relative flex h-screen w-screen flex-col bg:black md:items-center md:justify-center md:bg-transparent">
       <Head>
@@ -17,26 +35,28 @@ const login = () => {
 
        <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="logo" width={100} height={100} className={"absolute left-4 top-4 md:left-10 md:top-6        cursor-pointer object-contain"} />
 
-       <form className='relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14'>
+       <form onSubmit={handleSubmit(onSubmit)}  className='relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14'>
             <h1 className="text-xl font-semibold">Sign in</h1>
 
             <div className='space-y-4'>
                 <label className="inline-block w-full">
                     Email
-                    <input type="email" placeholder='Email' className='input'></input>
+                    <input {...register('email', {required: true})} type="email" placeholder='Email' className='input'></input>
+                    {errors.email && <p className=' text-sm text-orange-500 p-1'>Please enter a valid email</p>}
                 </label>
                 <label className=' inline-block w-full'>
                     Password
-                    <input type="password" placeholder='Password' className='input'></input>
+                    <input {...register('password', {required: true})} type="password" placeholder='Password' className='input'></input>
+                    {errors.password && <p className='text-sm text-orange-500 p-1'>Please enter a valid password</p>}
                 </label>
             </div>
 
 
-            <button type="submit" className="w-full rounded bg-[#e50914] py-3 font-semibold">Sign in</button>
+            <button type="submit" className="w-full rounded bg-[#e50914] py-3 font-semibold" onClick={() => setLogin (true)}>Sign in</button>
 
             <div className=' text-gray-500'>
                 New to Netflix? {" "}
-                <button className="text-white hover:underline">Sign up now</button>
+                <button className="text-white hover:underline" onClick={() => setLogin (false)}>Sign up now</button>
             </div>
        </form>
 
